fcb748119dac3e059dded8469deb6e7a419dd8cb
Zezuz <Mczanaty92@gmail.com>
2023-04-12 17:45:03 -0500

    Completed Flash chat

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
+
 
 class RegisterViewController: UIViewController {
-
+    
+    
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    //navigate to the Chat VC
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+            
+        }
+        
     }
-    
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Michael Zanaty on 3/26/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String //1@2.com will contain email addres
+    let body: String //body of the message
+    
+}
